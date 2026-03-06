d050f53a2f03dd188a84927b1a35176eb712254b
muratoren-fun <muratorentx@gmail.com>
2026-03-05 21:45:19 -0600

    Delete public directory

--- public/uv/uv.sw.js
+++
@@ -1,8 +0,0 @@
-/* Standard Ultraviolet Service Worker */
-importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.5/dist/uv.bundle.js');
-importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.5/dist/uv.config.js');
-importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.5/dist/uv.sw.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));

ee746d5cb6840cf3fd00177ec5069e76ba6311f2
muratoren-fun <muratorentx@gmail.com>
2026-03-05 21:32:05 -0600

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,10 +1,11 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: 'https://tomp.app/', 
+    /* Using a robust 2026 Bare server */
+    bare: 'https://uv.studentcenter.biz/bare/', 
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.5/dist/uv.handler.js',
     bundle: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.5/dist/uv.bundle.js',
     config: '/uv/uv.config.js',
-    sw: '/uv/uv.sw.js', // This must point to the file you just created
+    sw: '/uv/uv.sw.js',
 };
